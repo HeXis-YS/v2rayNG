9832921321b590316c2ddb5cfde9d4cc49eb522a
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2024-06-12 16:22:58 +0800

    VLAN IP

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/service/V2RayVpnService.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/service/V2RayVpnService.kt
@@ -26,10 +26,10 @@ import java.lang.ref.SoftReference
 class V2RayVpnService : VpnService(), ServiceControl {
     companion object {
         private const val VPN_MTU = 32767
-        private const val PRIVATE_VLAN4_CLIENT = "26.26.26.1"
-        private const val PRIVATE_VLAN4_ROUTER = "26.26.26.2"
-        private const val PRIVATE_VLAN6_CLIENT = "da26:2626::1"
-        private const val PRIVATE_VLAN6_ROUTER = "da26:2626::2"
+        private const val PRIVATE_VLAN4_CLIENT = "172.30.0.0"
+        private const val PRIVATE_VLAN4_ROUTER = "172.30.0.1"
+        private const val PRIVATE_VLAN6_CLIENT = "fd10::0"
+        private const val PRIVATE_VLAN6_ROUTER = "fd10::1"
         private const val TUN2SOCKS = "libtun2socks.so"
     }
 
@@ -198,7 +198,7 @@ class V2RayVpnService : VpnService(), ServiceControl {
         val socksPort = Utils.parseInt(settingsStorage?.decodeString(AppConfig.PREF_SOCKS_PORT), AppConfig.PORT_SOCKS.toInt())
         val cmd = arrayListOf(File(applicationContext.applicationInfo.nativeLibraryDir, TUN2SOCKS).absolutePath,
                 "--netif-ipaddr", PRIVATE_VLAN4_ROUTER,
-                "--netif-netmask", "255.255.255.252",
+                "--netif-netmask", "255.255.255.254",
                 "--socks-server-addr", "127.0.0.1:${socksPort}",
                 "--tunmtu", VPN_MTU.toString(),
                 "--sock-path", "sock_path",//File(applicationContext.filesDir, "sock_path").absolutePath,
